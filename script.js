91d898955cd4021640f185d12a08657944d1df31
Phawat <wpcreation2018@gmail.com>
2025-03-08 18:55:27 +0700

    Chapter 2: Completing and Deleting To-Dos (and Basic Styling)

--- script.js
+++ script.js
@@ -17,10 +17,35 @@ function addTodo() {
     const newTodoItem = document.createElement("li");
     newTodoItem.textContent = todoText; // Set the text content of the li
 
+    // Create a delete button
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    // Add event listener in delete button
+    deleteButton.addEventListener('click', deleteTodo);
+
+    // Add the delete button to the li
+    newTodoItem.appendChild(deleteButton);
+
+    // Add event listener for toggle complete in the newTodoItem
+    newTodoItem.addEventListener('click', toggelComplete);
+
     // Add the new li element to the ul
     todoList.appendChild(newTodoItem);
     // Clear the input field
     newTodoInput.value = "";
+}
+
+// Function to delete a to-do item
+function deleteTodo() {
+    // 'this' refers to the element that was clicked (the button)
+    // parentElement refers to the parent of the button (the li)
+    this.parentElement.remove();
+}
+
+// Function to toggle the 'completed' class on a to-do item
+function toggelComplete() {
+    // 'this' refers to the element that was clicked (the li)
+    this.classList.toggle('completed');
 }
 
 // Add an event listener to the button to call the addTodo function when clicked
